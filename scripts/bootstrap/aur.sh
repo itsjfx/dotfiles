a10eed0adc48ec2fc310e6ca7b15d80c0f20c475
Thomas <me@jfx.ac>
2023-01-27 00:36:59 +1100

    ghetto script for aur plugins i use

---
+++ scripts/bootstrap/aur.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+set -eu -o pipefail
+
+# i don't use an AUR manager so some ghetto shit
+# TODO: look into yay ?
+
+mkdir -p ~/aur/
+
+for package in "alttab-git" "cmusfm" "fnm" "joplin-appimage" "picom-git" "spotify-snapstore" "visual-studio-code-bin" "st"; do
+    git clone https://github.com/"$package" "$HOME"/aur/"$package"
+done
