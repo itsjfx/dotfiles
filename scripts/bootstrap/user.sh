e9c709db2308f2cb163b56a2a4e73736818fee96
Thomas <me@jfx.ac>
2023-05-22 22:45:51 +1000

    user script improvements
    can fetch from remote so it can be re run to update modules
    
    chsh only runs if not zsh already
    
    add to video group, if not in video group

--- scripts/bootstrap/user.sh
+++ scripts/bootstrap/user.sh
@@ -3,11 +3,30 @@ set -eu -o pipefail
 
 #curl -fsSL https://fnm.vercel.app/install | bash -s -- --skip-shell
 
-for plugin in "zsh-users/zsh-autosuggestions" "zsh-users/zsh-completions" "lincheney/fzf-tab-completion"; do
-    git clone https://github.com/"$plugin" "$HOME"/source/"${plugin##*/}"
+# bash-my-aws TODO
+for plugin in "zsh-users/zsh-autosuggestions" "zsh-users/zsh-completions" "lincheney/fzf-tab-completion" "itsjfx/zsh-tmux-smart-status-bar" "bash-my-aws/bash-my-aws"; do
+    plugin_name="${plugin##*/}"
+    plugin_path="$HOME/source/$plugin_name"
+
+    if cd "$plugin_path" 2>/dev/null; then
+        echo "Fetching $plugin_name" >&2
+        git fetch
+    else
+        echo "Cloning $plugin_name" >&2
+        git clone https://github.com/"$plugin" "$HOME"/source/"${plugin##*/}"
+    fi
 done
 
-chsh -s "$(which zsh)"
+if [[ "$(basename "$(grep "^${USER}" /etc/passwd | cut -f7 -d:)")" != 'zsh' ]]; then
+    chsh -s "$(which zsh)"
+else
+    echo 'Not changing shell' >&2
+fi
+
+if ! id -nGz "$USER" | grep -qzxF video; then
+    echo "Requesting sudo to add $USER to video group" >&2
+    sudo usermod -a -G video "$USER"
+fi
 
 kwriteconfig5 --file "$HOME"/.config/dolphinrc --group 'General' --key 'RememberOpenedTabs' false
 kwriteconfig5 --file "$HOME"/.config/dolphinrc --group 'General' --key 'BrowseThroughArchives' true
