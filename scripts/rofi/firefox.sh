a7eab6f7a1e057c2e541833f9791359cfaafa6e6
Thomas <me@jfx.ac>
2023-09-22 23:54:43 +1000

    rofi/i3: add script to open firefox profiles

---
+++ scripts/rofi/firefox.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# TODO python3 configparser ?
+# add icons
+# https://www.reddit.com/r/qtools/comments/dwadrk/rofiquestion_how_to_use_icons_in_scripts/f7l99ud/
+# https://github.com/davatorium/rofi/blob/a53daa68c4298fc64b17ea613705081c42c140c5/Examples/test_script_mode.sh#L24
+choice="$(awk -F "=" '/Name/ { output=$2"\0icon\x1ffirefox"; print output }' "$HOME"/.mozilla/firefox/profiles.ini | sort | rofi -dmenu -p '>')"
+if [[ -n "$choice" ]]; then
+    firefox -P "$choice" &
+fi
