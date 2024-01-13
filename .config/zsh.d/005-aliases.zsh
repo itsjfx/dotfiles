cc1926b932eca5bbe6186df6b4a64a8a9392a254
Thomas <me@jfx.ac>
2024-01-14 00:38:16 +1100

    fix dumb bug by me

--- .config/zsh.d/005-aliases.zsh
+++ .config/zsh.d/005-aliases.zsh
@@ -75,7 +75,7 @@ alias gap='git add --patch'
 alias cap='config add --patch'
 alias gb='git branch'
 alias gs='git status'
-_gcm() { (( $# > 1 )) && "$1" commit --message "$*" || "$1" commit }
+_gcm() { cmd="$1"; shift; (( $# > 0 )) && "$1" commit --message "$*" || "$1" commit }
 gcm() { _gcm git "$@" }
 ccm() { _gcm config "$@" }
 alias gl='git log --stat --patch'
