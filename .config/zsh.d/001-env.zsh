3c68fd699091dfddc95d5a07db4f5c3594f0168a
Thomas <me@jfx.ac>
2024-01-27 03:19:22 +1100

    friggin android sdk

--- .config/zsh.d/001-env.zsh
+++ .config/zsh.d/001-env.zsh
@@ -13,8 +13,10 @@ export PATH="$HOME/bin:$PATH"
 [ -d "$HOME/.meteor" ] && export PATH="$PATH:$HOME/.meteor"
 
 if [ -d "$HOME"/Android/Sdk ]; then
-	export ANDROID_SDK_ROOT="$HOME/Android/Sdk"
-	export PATH="$PATH:$HOME/Android/Sdk/platform-tools"
+    export ANDROID_SDK_ROOT="$HOME/Android/Sdk"
+    export ANDROID_HOME="$ANDROID_SDK_ROOT"
+    export PATH="$PATH:$ANDROID_HOME/platform-tools"
+    export PATH="$PATH:$ANDROID_HOME/cmdline-tools/latest/bin"
 fi
 
 # bma
