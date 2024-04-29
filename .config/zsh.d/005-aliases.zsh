6c11a9193a79ba06a6c3b6ad91376e156f226ea6
Thomas <me@jfx.ac>
2024-04-29 19:01:01 +1000

    call it tmp instead of gotmp and add a completion

--- .config/zsh.d/005-aliases.zsh
+++ .config/zsh.d/005-aliases.zsh
@@ -99,7 +99,7 @@ sshkey() { cat "$HOME"/.ssh/id_ed25519.pub; }
 
 sumup() { awk '{s+=$1} END {print s}'; }
 
-gotmp() { mkdir -p /tmp/"$1"; cd /tmp/"$1"; }
+tmp() { mkdir -p /tmp/"$1"; cd /tmp/"$1"; }
 
 alias mkssh='ssh-keygen -t ed25519'
 
