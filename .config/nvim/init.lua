20ec0abfa82edd4878d34d5c4f18a5a3c4e92f10
Thomas <me@jfx.ac>
2024-08-08 23:24:28 +1000

    vim move

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1010,7 +1010,11 @@ require('lazy').setup({
         return '%2l:%-2v'
       end
 
+      -- Hovering over a word to show other matches
       require('mini.cursorword').setup()
+
+      -- Moving stuff around easily
+      require('mini.move').setup()
       -- ... and there is more!
       --  Check out: https://github.com/echasnovski/mini.nvim
     end,
