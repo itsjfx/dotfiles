c5bb87d98144413d030187cfc0ec34d271efa7c7
Thomas <me@jfx.ac>
2024-08-31 22:26:13 +1000

    bad git fido2 script that only works on sandcastle

---
+++ lib/git-test.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+# https://git-scm.com/docs/git-config#Documentation/git-config.txt-gpgsshdefaultKeyCommand
+
+[ -e /dev/tty ] && exec 2>/dev/tty
+
+
+# host="$(git remote get-url origin | sed 's/.*@//' | sed 's/:.*//')"
+# echo "$host" >&2
+if [ -n "$SSH_CLIENT" ]; then
+    if match="$(<<<"$(ssh-add -L)" grep -m 1 bazooka)"; then
+        :
+    elif ! match="$(<<<"$(ssh-add -L)" grep -m 1 'github@keychain')"; then
+        echo "Could not find keychain key in SSH Agent" >&2
+        exit 1
+    fi
+else
+    if ! match="$(<<<"$(ssh-add -L)" grep -m 1 -E "github@$(hostname)$")"; then
+        echo "Could not find key in SSH Agent" >&2
+        exit 1
+    fi
+fi
+echo "Signing with key: $(echo "$match" | cut -f3 -d ' ')" >&2
+echo "key::$match"
