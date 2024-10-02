dd10772133bb9c53acbd595bdf194583a2d32b5c
Thomas <me@jfx.ac>
2024-10-02 15:33:37 +1000

    mediainfo with colours

--- lib/cmus-mediainfo.sh
+++ lib/cmus-mediainfo.sh
@@ -4,4 +4,5 @@ set -eu -o pipefail
 
 track="$(cmus-remote -C 'echo {}')"
 cmus-remote -C 'echo x'
-mediainfo "$track" | less
+export _FORCE=1
+mediainfo "$track" | sed 's/ \+: /: /' | sed 's/: /\t/' | "$HOME"/bin/columnise_pretty | bat
