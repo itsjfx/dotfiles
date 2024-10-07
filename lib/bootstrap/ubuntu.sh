19085dfb7d36fefb2f1db5255bb4a0869226a23e
Thomas <me@jfx.ac>
2024-10-07 11:52:18 +1100

    ubuntu bootstrap

---
+++ lib/bootstrap/ubuntu.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+set -eu -o pipefail
+
+# sudo sed -E -i 's/http:\/\/(archive|security).ubuntu.com/https:\/\/mirror.gsl.icu/' /etc/apt/sources.list
+sudo apt update
+sudo apt upgrade
+sudo apt install ca-certificates neovim git colordiff whois jq zstd zip unzip bc gron fzf bat ncdu moreutils socat python3-pip magic-wormhole
