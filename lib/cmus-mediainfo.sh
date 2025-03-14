29f309e794d2d6ab7770a2bf9aeb96b9417e17ec
Thomas <me@jfx.ac>
2025-03-14 22:04:26 +1100

    always page mediainfo in cmus

--- lib/cmus-mediainfo.sh
+++ lib/cmus-mediainfo.sh
@@ -5,4 +5,4 @@ set -eu -o pipefail
 track="$(cmus-remote -C 'echo {}')"
 cmus-remote -C 'echo x'
 export _FORCE=1
-mediainfo "$track" | sed 's/ \+: /: /' | sed 's/: /\t/' | "$HOME"/bin/columnise_pretty | bat
+mediainfo "$track" | sed 's/ \+: /: /' | sed 's/: /\t/' | "$HOME"/bin/columnise_pretty | bat --paging=always
