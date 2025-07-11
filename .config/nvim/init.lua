524b55ce7763950255cdfc6fb1f1f902b35d9fdb
Thomas <me@jfx.ac>
2025-07-11 22:55:06 +1000

    nvim: fix lsps not being configured with kickstart lspconfig

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -943,86 +943,92 @@ require('lazy').setup({
       --  - capabilities (table): Override fields in capabilities. Can be used to disable certain LSP features.
       --  - settings (table): Override the default settings passed when initializing the server.
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
+      ---@class LspServersConfig
+      ---@field mason table<string, vim.lsp.Config>
+      ---@field others table<string, vim.lsp.Config>
       local servers = {
-        basedpyright = {
-          settings = {
-            basedpyright = {
-              analysis = {
-                typeCheckingMode = 'off',
+        mason = {
+          basedpyright = {
+            settings = {
+              basedpyright = {
+                analysis = {
+                  typeCheckingMode = 'off',
+                },
               },
             },
           },
-        },
-        yamlls = {
-          settings = {
-            yaml = {
-              customTags = {
-                '!And',
-                '!If',
-                '!Not',
-                '!Equals',
-                '!Or',
-                '!FindInMap sequence',
-                '!Base64',
-                '!Cidr',
-                '!Ref',
-                '!Sub',
-                '!GetAtt',
-                '!GetAZs',
-                '!ImportValue',
-                '!Select',
-                '!Select sequence',
-                '!Split',
-                '!Join sequence',
+          yamlls = {
+            settings = {
+              yaml = {
+                customTags = {
+                  '!And',
+                  '!If',
+                  '!Not',
+                  '!Equals',
+                  '!Or',
+                  '!FindInMap sequence',
+                  '!Base64',
+                  '!Cidr',
+                  '!Ref',
+                  '!Sub',
+                  '!GetAtt',
+                  '!GetAZs',
+                  '!ImportValue',
+                  '!Select',
+                  '!Select sequence',
+                  '!Split',
+                  '!Join sequence',
+                },
               },
             },
           },
-        },
-        -- clangd = {},
-        -- gopls = {},
-        -- pyright = {},
-        -- rust_analyzer = {},
-        -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
-        --
-        -- Some languages (like typescript) have entire language plugins that can be useful:
-        --    https://github.com/pmizio/typescript-tools.nvim
-        --
-        -- But for many setups, the LSP (`tsserver`) will work just fine
-        ts_ls = {
-          settings = {
-            javascript = {
-              format = {
-                enable = false,
+          -- clangd = {},
+          -- gopls = {},
+          -- pyright = {},
+          -- rust_analyzer = {},
+          -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
+          --
+          -- Some languages (like typescript) have entire language plugins that can be useful:
+          --    https://github.com/pmizio/typescript-tools.nvim
+          --
+          -- But for many setups, the LSP (`tsserver`) will work just fine
+          ts_ls = {
+            settings = {
+              javascript = {
+                format = {
+                  enable = false,
+                },
               },
-            },
-            typescript = {
-              format = {
-                enable = false,
+              typescript = {
+                format = {
+                  enable = false,
+                },
               },
             },
           },
-        },
-        --
+          --
 
-        lua_ls = {
-          -- cmd = {...},
-          -- filetypes = { ...},
-          -- capabilities = {},
-          settings = {
-            Lua = {
-              completion = {
-                callSnippet = 'Replace',
+          lua_ls = {
+            -- cmd = {...},
+            -- filetypes = { ...},
+            -- capabilities = {},
+            settings = {
+              Lua = {
+                completion = {
+                  callSnippet = 'Replace',
+                },
+                -- You can toggle below to ignore Lua_LS's noisy `missing-fields` warnings
+                -- diagnostics = { disable = { 'missing-fields' } },
               },
-              -- You can toggle below to ignore Lua_LS's noisy `missing-fields` warnings
-              -- diagnostics = { disable = { 'missing-fields' } },
             },
           },
+          clangd = {},
+          html = {},
+          gopls = {},
+          kotlin_lsp = {},
+          -- ruff = {},
         },
-        clangd = {},
-        html = {},
-        gopls = {},
-        kotlin_lsp = {},
-        -- ruff = {},
+        others = {},
       }
 
       -- Ensure the servers and tools above are installed
@@ -1035,14 +1041,24 @@ require('lazy').setup({
 
       -- You can add other tools here that you want Mason to install
       -- for you, so that they are available from within Neovim.
-      local ensure_installed = vim.tbl_keys(servers or {})
+      local ensure_installed = vim.tbl_keys(servers.mason or {})
       vim.list_extend(ensure_installed, {
         'stylua', -- Used to format Lua code
       })
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
+      -- Either merge all additional server configs from the `servers.mason` and `servers.others` tables
+      -- to the default language server configs as provided by nvim-lspconfig or
+      -- define a custom server config that's unavailable on nvim-lspconfig.
+      for server, config in pairs(vim.tbl_extend('keep', servers.mason, servers.others)) do
+        if not vim.tbl_isempty(config) then
+          vim.lsp.config(server, config)
+        end
+      end
+
       require('mason-lspconfig').setup {
         ensure_installed = {}, -- explicitly set to an empty table (Kickstart populates installs via mason-tool-installer)
+        automatic_enable = true,
         automatic_installation = false,
         handlers = {
           function(server_name)
@@ -1055,6 +1071,10 @@ require('lazy').setup({
           end,
         },
       }
+      -- Manually run vim.lsp.enable for all language servers that are *not* installed via Mason
+      if not vim.tbl_isempty(servers.others) then
+        vim.lsp.enable(vim.tbl_keys(servers.others))
+      end
     end,
   },
 
