c3f5d9e9049a9fff3b827264e275ffc1c7584f3e
Thomas <me@jfx.ac>
2025-07-14 16:02:22 +1000

    install some additional things when bootstrapping ubuntu

--- lib/bootstrap/ubuntu.sh
+++ lib/bootstrap/ubuntu.sh
@@ -23,7 +23,12 @@ sudo DEBIAN_FRONTEND=noninteractive apt install -y \
     moreutils \
     socat \
     python3-pip \
-    magic-wormhole \
 
 git clone --depth 1 https://github.com/junegunn/fzf.git "$HOME"/.fzf
 ln -srf "$HOME"/.fzf/bin/fzf "$HOME"/bin/
+
+pip install uv
+
+curl -L "$(curl -sSL "https://api.github.com/repos/dandavison/delta/releases/latest" | jq -r '.assets[] | select(.browser_download_url | endswith("amd64.deb")).browser_download_url' | grep -v musl)" -o /tmp/delta.deb
+
+sudo dpkg -i /tmp/delta.deb
