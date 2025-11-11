0637cc95546df4ed3fe3348d7cc71971efd8d39e
Thomas <me@jfx.ac>
2025-11-11 13:05:38 +1100

    some stuff

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -956,7 +956,15 @@ require('lazy').setup({
           },
           yamlls = {
             settings = {
+              redhat = {
+                telemetry = {
+                  enabled = false,
+                },
+              },
               yaml = {
+                format = {
+                  singleQuote = true,
+                },
                 customTags = {
                   '!And',
                   '!If',
@@ -1108,7 +1116,8 @@ require('lazy').setup({
         -- Disable "format_on_save lsp_fallback" for languages that don't
         -- have a well standardized coding style. You can add additional
         -- languages here or re-enable it for the disabled ones.
-        local disable_filetypes = { c = true, cpp = true, javascript = true, typescript = true, js = true, ts = true, kotlin = true, kt = true, css = true, scss = true }
+        local disable_filetypes =
+          { c = true, cpp = true, javascript = true, typescript = true, js = true, ts = true, kotlin = true, kt = true, css = true, scss = true }
         if disable_filetypes[vim.bo[bufnr].filetype] then
           return nil
         else
