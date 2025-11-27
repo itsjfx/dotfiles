1593ace4cdcd57490f7fa73b0d4fc00ad9f98696
Thomas <me@jfx.ac>
2025-11-27 23:20:25 +1100

    pretty columnising

--- lib/cmus-mediainfo.sh
+++ lib/cmus-mediainfo.sh
@@ -4,5 +4,4 @@ set -eu -o pipefail
 
 track="$(cmus-remote -C 'echo {}')"
 cmus-remote -C 'echo x'
-export _FORCE=1
-mediainfo "$track" | sed 's/ \+: /: /' | sed 's/: /\t/' | "$HOME"/bin/columnise_pretty | bat --paging=always
+mediainfo "$track" | sed 's/ \+: /: /' | sed 's/: /\t/' | "$HOME"/bin/pretty --columnize=always --color=always | bat --paging=always
